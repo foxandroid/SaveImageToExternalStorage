624c4196650b6183b9481d97ddd759d2e0ed630a
robin216 <37136276+robin216@users.noreply.github.com>
2020-11-23 13:53:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/saveimagetoexternalstorage/MainActivity.java
@@ -0,0 +1,124 @@
+package com.example.saveimagetoexternalstorage;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.security.Permission;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int REQUEST_CODE = 100;
+    ImageView bird;
+    Button SaveImg;
+    OutputStream outputStream;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SaveImg = findViewById(R.id.saveimgbtn);
+        bird = findViewById(R.id.bird);
+        SaveImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+
+                    saveImage();
+
+                }else {
+
+
+                    askPermission();
+
+                }
+
+            }
+        });
+    }
+
+    private void askPermission() {
+
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE)
+        {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+
+                saveImage();
+
+            }else {
+
+
+                Toast.makeText(MainActivity.this,"Please provide the required permissions",Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void saveImage() {
+
+        File dir = new File(Environment.getExternalStorageDirectory(),"SaveImage");
+
+        if (!dir.exists()){
+
+            dir.mkdir();
+
+        }
+
+        BitmapDrawable drawable = (BitmapDrawable) bird.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        File file = new File(dir,System.currentTimeMillis()+".jpg");
+        try {
+            outputStream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+        Toast.makeText(MainActivity.this,"Successfuly Saved",Toast.LENGTH_SHORT).show();
+
+        try {
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
